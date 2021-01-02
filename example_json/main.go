f20a2a3cf88d78f52a2a8466f7f2fd35300cb2cf
Nicola Solazzo <nicola.solazzo@starteed.com>
2021-01-02 15:32:01 +0100

    Marshall Ident Json

--- example_json/main.go
+++ example_json/main.go
@@ -7,13 +7,29 @@ import (
 
 type Book struct {
 	Title  string `json:"title"`
-	Author string `json:"author"`
-	Info   string `json:"info"`
+	Author Author `json:"author"`
+	Info   Info   `json:"info"`
+}
+
+type Author struct {
+	Nickname  string `json:"nickname"`
+	Age       int    `json:"age"`
+	Developer bool   `json:"is_developer"`
+	Skill     string `json:"skill"`
+}
+
+type Info struct {
+	Price       float32 `json:"price"`
+	Pages       int     `json:"pages"`
+	Description string  `json:"description"`
 }
 
 func main() {
-	book := Book{Title: "Learn Go with joy", Author: "Nicola Solazzo", Info: "this book is Free"}
-	byteArray, err := json.Marshal(book)
+	author := Author{Nickname: "Nicola Solazzo", Age: 44, Developer: true, Skill: "Golang"}
+	info := Info{Price: 29.99, Pages: 320, Description: "This book is un fake integration to try your skill with Golang"}
+	book := Book{Title: "Learn Go with joy", Author: author, Info: info}
+
+	byteArray, err := json.MarshalIndent(book, "", " ")
 	if err != nil {
 		fmt.Println(err)
 	}
