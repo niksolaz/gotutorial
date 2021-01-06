c5f72693d748f7c3d40bc69c6663fd66a66da143
Nicola Solazzo <solazzo.nicola@gmail.com>
2021-01-06 12:24:39 +0100

    add module os for open file

--- example_read_file_json/main.go
+++ example_read_file_json/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+)
 
+func main() {
+	jsonfile, err := os.Open("info.json")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Json file opened")
+
+	defer jsonfile.Close()
 }
